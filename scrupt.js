42eb7da1c787b6644fa1c7f6afdfa3b035758daa
smash3367 <103372957+smash3367@users.noreply.github.com>
2023-08-20 19:42:31 +0400

    Add files via upload

---
+++ scrupt.js
@@ -0,0 +1,5 @@
+let registaration = document.querySelector('.header-account');
+let main = document.querySelector('main');
+registaration.addEventListener('click', function() {
+    main.innerHTML('');
+});
